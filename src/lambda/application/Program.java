55383ee1af345de688c03596aa32c8deda58edbb
RaposoBrendo <raposobrendo@gmail.com>
2026-04-20 17:06:26 -0300

    Initiating Functional Programming and Lambda Expressions.

---
+++ src/lambda/application/Program.java
@@ -0,0 +1,43 @@
+package lambda.application;
+
+import lambda.model.entities.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program {
+    public static void main(String[] Args){
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("Tv", 900.0));
+        list.add(new Product("Notebook", 1200.00));
+        list.add(new Product("Tablet", 450.00));
+
+        /*Comparator<Product> comp = new Comparator<Product>() {
+            @Override
+            public int compare(Product p1, Product p2) {
+                return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+            }
+        };
+        This will work, First Comparator */
+
+        /*Comparator<Product> comp = (p1, p2) -> {
+            return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+        };
+        This will work. Second Comparator, using arrow function and return. */
+
+        /*Comparator<Product> comp = (p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+        This will work. Third Comparator, using arrow function, without return */
+
+        // list.sort(new MyComparator()); This will work.
+
+
+        list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
+
+        for(Product p : list){
+            System.out.println(p);
+        }
+
+    }
+}
